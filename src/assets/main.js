bf6f69bd9ec36485cae6824f059028dec5b38bf3
Jose Manuel <j.ochoa317@gmail.com>
2023-01-24 12:11:24 -0500

    UI - Find Pokemon III

--- src/assets/main.js
+++ src/assets/main.js
@@ -2,13 +2,67 @@ const boxPokemon=document.getElementById('boxPokemon');
 const btnKanto=document.getElementById('kanto');
 const btnJohto=document.getElementById('johto');
 
+let counter=0;
+
+let options={
+    threshold: 1.0
+}
+
+const observer=new IntersectionObserver(loadMoreBox,options);
+function loadMoreBox(entry){
+    if(entry[0].isIntersecting){
+        fetchData(`${API}pokedex/2`,10);
+    }
+}
+
+async function loadBoxPokemon(pokename){
+    let imgpokemon;
+    try {
+        let response=await fetch(`https://pokeapi.co/api/v2/pokemon/${pokename}`);
+        let data=await response.json()
+        imgpokemon=data.sprites.front_default;
+    } catch (error) {
+        imgpokemon='./assets/images/poke.png';
+    }
+
+    const container=document.createElement("DIV");
+    container.classList.add('text-center','p-1','sm:p-4','shadow-[0px_0px_5px_0px_gray]','rounded-lg','w-[130px]','sm:w-[160px]','min-[920px]:w-[200px]','border-box');
+    const firstChild=document.createElement("DIV");
+    const pokeimage=document.createElement("IMG");
+    pokeimage.classList.add('m-auto');
+    const secondChild=document.createElement("DIV");
+    secondChild.classList.add('font-bold','capitalize');
+    const button=document.createElement("BUTTON");
+    button.classList.add('bg-red-400','hover:bg-red-500','mt-2','py-1','w-full','rounded-lg');
+
+    pokeimage.setAttribute('src',`${imgpokemon}`);
+    
+    secondChild.textContent=pokename;
+    button.textContent="More Info";
+
+    firstChild.appendChild(pokeimage);
+    container.appendChild(firstChild);
+    container.appendChild(secondChild);
+    container.appendChild(button);
+
+    return container;
+}
+
+
+
 const API='https://pokeapi.co/api/v2/';
-async function fetchData(urlAPI){
-    boxPokemon.innerHTML='';
+async function fetchData(urlAPI,num){
     const response=await fetch(urlAPI);
     const data=await response.json();
-    console.log(data)
-    for(const pokeName of data.pokemon_entries){
+    console.log(data.pokemon_entries)
+    for (let i = 0; i < num ; i++) {
+        if(counter > data.pokemon_entries.length-1) {break}
+        const newBox= await loadBoxPokemon(data.pokemon_entries[counter].pokemon_species.name);
+        boxPokemon.appendChild(newBox);
+        counter++;
+        if(i == num-1) {observer.observe(boxPokemon.lastChild);}
+    }
+    /* for(const pokeName of data.pokemon_entries){
         await fetch(`https://pokeapi.co/api/v2/pokemon/${pokeName.pokemon_species.name}`)
             .then(pokeResponse=>pokeResponse.json())
             .then(pokeData=>{
@@ -33,14 +87,18 @@ async function fetchData(urlAPI){
                 </div>
                 `)
             })
-    }
+    } */
 }
 //pokemon?offset=60&limit=60
-fetchData(`${API}pokedex/1`);
+fetchData(`${API}pokedex/2`,10);
 
 btnKanto.addEventListener("click",()=>{
-    fetchData(`${API}pokedex/2`)
+    counter=0;
+    boxPokemon.innerHTML=''
+    fetchData(`${API}pokedex/2`,10)
 })
 btnJohto.addEventListener("click",()=>{
-    fetchData(`${API}pokedex/3`)
+    counter=0;
+    boxPokemon.innerHTML=''
+    fetchData(`${API}pokedex/3`,10)
 })
